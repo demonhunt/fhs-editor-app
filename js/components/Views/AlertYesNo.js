dc0b74bf0845aa80a31ecc8ba4edd8df86b5e070
khuong <khuong@localhost.localdomain>
2019-07-02 08:19:06 +0700

    UI home menu

---
+++ js/components/Views/AlertYesNo.js
@@ -0,0 +1,160 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet, TouchableOpacity, Modal } from 'react-native'
+import globalSetting from '../../common/setting'
+
+export default class AlertYesNo extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <Modal
+        animationType={'none'}
+        transparent={true}
+        visible={this.props.isShowAlertView}
+        onRequestClose={() => {}}
+      >
+        <View style={styles.container}>
+          <View
+            style={{
+              width: 260,
+              backgroundColor: 'white',
+              justifyContent: 'space-around',
+              alignItems: 'center',
+              borderRadius: 10,
+            }}
+          >
+            <View
+              style={{
+                alignItems: 'center',
+                margin: 10,
+                paddingLeft: 10,
+                paddingRight: 10,
+              }}
+            >
+              <Text
+                style={{
+                  fontWeight: 'bold',
+                  fontSize: 15,
+                  textAlign: 'center',
+                  color: globalSetting.main_text_color,
+                }}
+              >
+                {this.props.title ? this.props.title : 'Thông báo'}
+              </Text>
+            </View>
+            <View
+              style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                paddingLeft: 10,
+                paddingRight: 10,
+                paddingTop: 10,
+                paddingBottom: 20,
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: 13,
+                  textAlign: 'center',
+                  color: globalSetting.main_text_color,
+                  lineHeight: 20,
+                }}
+              >
+                {this.props.message}
+              </Text>
+            </View>
+            <View style={{ flexDirection: 'row' }}>
+              <View
+                style={{
+                  flex: 1,
+                  borderTopWidth: 0.5,
+                  borderTopColor: globalSetting.drawer_line_color,
+                }}
+              />
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                height: 50,
+                //justifyContent: 'center',
+                //alignItems: 'center',
+              }}
+            >
+              <View
+                style={{
+                  flex:1,
+                  //alignItems: 'center',
+                  //justifyContent: 'center',
+                  flexDirection: 'row',
+                  //backgroundColor : 'blue'
+                  
+                }}
+              >
+              <TouchableOpacity
+                style={{
+                  flex: 1,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+                onPress={() => {
+                    
+                    if (this.props.callBack) {
+                     this.props.callBlackLogout()
+                    }
+                    
+                 }}
+              >
+                 <Text
+                    style={{
+                    color: globalSetting.main_orange_color,
+                    fontSize: 15,
+                    }}    
+                >
+                    YES
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                    flex: 1,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    //backgroundColor: 'red',   
+                }}
+                onPress={() => {
+                    setTimeout(() => {
+                    if (this.props.callBack) {
+                        this.props.callBack()
+                    }
+                },0)
+                }}
+              >
+                 <Text
+                    style={{
+                    color: globalSetting.main_orange_color,
+                    fontSize: 15,
+                    }}
+                >
+                   NO
+                </Text>
+              </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </View>
+      </Modal>
+    )
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'rgba(52,52,52,0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
--- js/components/Views/HomeStack/HomeMain.js
+++ js/components/Views/HomeStack/HomeMain.js
@@ -11,8 +11,66 @@ import {
 } from "react-native";
 import { connect } from "react-redux";
 import { logout } from "../../../actions";
-import globalSetting from "../../../common/setting"
-import { PermissionsAndroid } from "react-native";
+import globalSetting from "../../../common/setting";
+import AlertYesNo from '../AlertYesNo';
+import { PermissionsAndroid} from "react-native";
+import ImageUtil from '../../../common/image/ImageUtil';
+
+const arrayButton = [
+  {
+    title: "Scan",
+    icon: "SKU",
+    navigation : 'scanSKU',
+  },
+  {
+    title: "chuyen ke",
+    icon: "pack",
+    navigation : 'pack',
+  },
+  {
+    title: "Quan ly ke",
+    icon: "tray",
+    navigation : 'tray',
+  },
+  {
+    title: "Nhap sach len ke",
+    icon: "truck",
+    navigation : 'truck',
+  },
+  {
+    title: "Nhap bang phieu",
+    icon: "truck",
+    navigation : 'truck',
+  },
+  {
+    title: "Nhap bang phieu",
+    icon: "truck",
+    navigation : 'truck',
+  },
+]
+class GetAllDataList extends Component {
+  render(){
+    const {item} = this.props;
+    return(
+      <View style={styles.contentFlaxListItem}>
+        <TouchableOpacity
+          onPress = {()=>{
+          this.props.navigation.navigate(item.navigation);
+        }}>
+          <Image
+            source={ImageUtil.getImageSource(item.icon)}
+            style={{ width: 30, height: 30, alignSelf: 'center'}}
+          />
+          <Text style={{ 
+            fontSize: 15,
+            textAlign: 'center',
+            marginTop: 5,
+            color:'black'}}>{item.title}</Text>
+        </TouchableOpacity>
+      </View>  
+    );
+  }
+}
 export default class HomeMain extends Component {
   static navigationOptions = ({ navigation }) => {
     return {
@@ -21,7 +79,10 @@ export default class HomeMain extends Component {
   };
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      isShowAlertView: false,
+      message: this.props.user.message,
+    };
   }
 
   componentDidMount() {
@@ -50,47 +111,142 @@ export default class HomeMain extends Component {
   postLogout() {
     this.props.dispatch(logout());
   }
-
+  userLogout(){
+    if(this.state.isShowAlertView == false)
+    this.setState({ isShowAlertView: true });
+    // this.props.dispatch({
+    //   type: 'LOGOUT_SUCCESS'
+    // })
+  }
   render() {
+    //console.log(this.props.user.message);
+    //console.log(this.props.user);
     return (
-      <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}>
-        <Text>Welcome</Text>
-
-        <TouchableOpacity style={{
-          backgroundColor: globalSetting.main_orange_color,
-          padding: 20,margin: 20
+      <View style={styles.content}>
+        <AlertYesNo
+            isShowAlertView={this.state.isShowAlertView}
+            callBack={() => {
+              this.setState({ isShowAlertView: false });
+            }}
+            callBlackLogout={()=>{ this.props.dispatch({
+                type: 'LOGOUT_SUCCESS'})
+            }}
+            message="Are you sure?"
+          />
+      <View style={styles.contentHeader}>
+        <View style={{
+          flex:1,
+          flexDirection: 'row',
+        }}>
+        <View style={{
+          flex:1,
+          flexDirection: 'row',
+          alignItems: 'center',
+        }}>
+        <Image
+            source={ImageUtil.getImageSource('setting')}
+            style={{ width: 30, height: 30, marginLeft: 5, marginRight: 5 }}
+        />
+        <Text style={{fontSize:15,color:'black'}}>{this.props.user.userInfor.lastName}</Text>
+        </View>
+        <View style={{flex:1}}>
+        <TouchableOpacity 
+          onPress = {()=>{
+            this.userLogout()
         }}
-        onPress = {()=>{
-          this.props.dispatch({
-            type: 'LOGOUT_SUCCESS'
-          })
-        }}
-        
+          style={styles.iconButtonLogout}
         >
-          <Text>Logout</Text>
+        <Text style={{fontSize:15,color: 'black', alignSelf:'center'}}>Logout</Text> 
+        <Image
+            source={ImageUtil.getImageSource('checkout')}
+            style={{width: 30, height: 30, marginLeft: 5, marginRight: 5,}}
+        />
         </TouchableOpacity>
-        <TouchableOpacity style={{
-          backgroundColor: globalSetting.main_orange_color,
-          padding: 20,margin: 20
-        }}
-        onPress = {()=>{
-          this.props.navigation.navigate('scanSKU');
-        }}
-        
-        >
-          <Text>Scan</Text>
-        </TouchableOpacity>
+        </View>
+        </View>
+        <View style={styles.contentTitle}><Text style={{  
+          fontSize: 20, 
+          color: 'white',
+          fontWeight: 'bold',
+        }}>Thuong Mai Dien Tu Fahasa</Text></View>
       </View>
+      <View style={styles.contentFlaxList}>
+        <FlatList
+          data={arrayButton}
+          renderItem={({item})=>{
+            return <GetAllDataList 
+            item={item}
+            navigation={this.props.navigation}
+            />   
+          }}
+          keyExtractor={(item, index) => {
+                        return index.toString();
+          }}
+          numColumns = {'2'}
+          showsVerticalScrollIndicator={false}
+        />
+      </View>
+      
+      
+    </View>
     );
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  content:{
+    flex:1,
+    backgroundColor: globalSetting.gray
+  },
+  contentHeader:{
+    //flex: 1,
+    flexDirection: 'column',
+    justifyContent:'center',
+    //alignItems: 'center',
+    //paddingTop : 10,
+    height: 100,
+    backgroundColor: 'white',
+  },
+  contentTitle:{
+    backgroundColor: globalSetting.main_orange_color, 
+    flex:1,
+    flexDirection : 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  contentFlaxList:{
+    flex: 1,
+    //justifyContent: 'flex-end',
+    //alignItems: 'center',
+    //textAlign: 'center',
+    //alignContent : 'center',
+    backgroundColor: 'white',
+    //marginTop: 30,
+    //marginBottom: 300,
+
+  },
+  contentFlaxListItem:{
+    flex: 1,
+    //backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    //flexDirection : 'column',
+    //alignContent: 'center',
+    //width: 150,
+    marginTop : 20,
+    
+  },
+  iconButtonLogout:{
+    flex:1,
+    flexDirection: 'row', 
+    justifyContent:'flex-end',
+    //alignContent : 'center',
+    alignItems: 'center',
+    alignSelf:'flex-end',
+    //backgroundColor: 'red'
+  },
+
+});
 function select(store) {
   return {
     user: store.user
